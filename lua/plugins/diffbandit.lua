92040115ae1dca19849a76544de8ebeeaafd7588
Mike Deiters <mike@deiters.me>
2026-08-12 18:43:41 -0400

    Update keybinds for diffbandit

--- lua/plugins/diffbandit.lua
+++ lua/plugins/diffbandit.lua
@@ -4,7 +4,8 @@ return {
     config = function()
       local diffbandit = require("diffbandit")
       diffbandit.setup()
-      vim.keymap.set("n", "<leader>gd", "<cmd>DiffBanditGit<CR>")
+      vim.keymap.set("n", "<M-0>", "<cmd>DiffBanditGit<CR>")
+      vim.keymap.set("n", "<M-z>", "<cmd>DiffBanditDiscardHunk<CR>")
     end,
   },
 }
